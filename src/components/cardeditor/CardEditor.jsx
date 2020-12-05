7aa4037dca2225ae4823d439da43b3c5dc074361
mrcau <mrcau98@gmail.com>
2020-12-06 00:03:00 +0900

    'edit'

---
+++ src/components/cardeditor/CardEditor.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CardEditor = ({card}) => (
+      <h1>{card.id}</h1>
+  );
+
+export default CardEditor;
--- src/components/editor/Editor.jsx
+++ src/components/editor/Editor.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import CardEditor from '../cardeditor/CardEditor';
 import style from './editor.module.css';
 const Editor = ({ card }) => (
   <section className={style.editor}>
     <h1>EDITOR</h1>
+    {card.map((card) => {return(<CardEditor card={card}/>)
+      
+    })}
   </section>
 );
 
