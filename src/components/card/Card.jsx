6eb5ea5cfdadba9e01675864426f0c85b3fce664
mrcau <mrcau98@gmail.com>
2020-12-06 09:10:49 +0900

    'dd'

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
-const Card = ({ card }) => (
-      <h1>{card.id}</h1>
-  );
+const Card = ({ card }) => {
+  const {id,name,company,title,email,message,theme,filename,fileurl}=card;
+  const defaultIMG = '/image/default_logo.png';
+  const url = fileurl || defaultIMG;
+  return(
+      <div>
+        <img src={url} alt="face"/>
+        <div>
+          <h1>{name}</h1>
+          <p>{company}</p>
+          <p>{title}</p>
+          <p>{email}</p>
+          <p>{message}</p>
+        </div>
+      </div>
+  );}
 
 export default Card;
